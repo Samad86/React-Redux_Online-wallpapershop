57ebcdb82986cec44be287bf23e85dec2537d596
samad86 <mads198644@gmail.com>
2020-09-27 22:09:51 +0300

    Асинхронные экшены (Redux Thunk)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
-import axios from "axios";
 import { useDispatch } from "react-redux";
 
 import { Header } from "./components"; // webpack при сборке сначала ищет файл index.js по указанному пути
 import { Home, Cart } from "./pages";
-import { setWallpapers } from "./redux/actions/wallpapers";
+import { fetchWallpapers } from "./redux/actions/wallpapers";
 
 // Классовый компонент
 // class App extends React.Component {
@@ -57,10 +56,8 @@ function App() {
   const dispatch = useDispatch(); // useDispatch - получение функции store.dispatch в компоненте (аналог mapDispatchToProps в классовом компоненте). Хук возвращает ссылку на dispatch функцию из Redux. Используется для отправки действий.
 
   useEffect(() => {
-    axios.get("http://localhost:3001/wallpapers").then(({ data }) => {
-      dispatch(setWallpapers(data));
-    });
-  }, []); // выполнение эффекта только 1 раз при первом рендере
+    dispatch(fetchWallpapers());
+  }, []); // выполнение эффекта только 1 раз при первом рендере (отправка экшена). dispatch выполняет асинхронный экшен fetchWallpapers, который возвращает функцию
 
   return (
     <div className="wrapper">
--- src/redux/actions/wallpapers.js
+++ src/redux/actions/wallpapers.js
@@ -1,4 +1,13 @@
+import axios from "axios";
+
+// fetchWallpapers - асинхронный экшен (Reux Thunk)
+export const fetchWallpapers = () => (dispatch) => {
+  axios.get("http://localhost:3001/wallpapers").then(({ data }) => {
+    dispatch(setWallpapers(data));
+  });
+}; // функция fetchWallpapers возвращает другую функцию. Вторая функция выполняется с помощью библиотеки Redux Thunk (для получения товаров (запрос на сервер) и сохранения товаров)
+
 export const setWallpapers = (items) => ({
   type: "SET_WALLPAPERS",
   payload: items,
-});
+}); // функция setWallpapers возвращает объект (для сохранения товаров)
